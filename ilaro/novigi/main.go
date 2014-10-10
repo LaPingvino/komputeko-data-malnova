358448c89a9219cb289361c772b18005d6171cfc
Joop Kiefte <joop@kiefte.net>
2014-10-10 18:59:03 +0200

    Command ready, complete json added to repository

--- ilaro/novigi/main.go
+++ ilaro/novigi/main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
-	"flag"
 	"bufio"
 	"bytes"
-	"strings"
-	"regexp"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
-	"encoding/json"
+	"regexp"
+	"strings"
 )
 
-type Terminaro	[]Entry
-type Entry	struct {
-	Wordtype	string
-	Translations	[]Translation
+type Terminaro []Entry
+type Entry struct {
+	Wordtype     string
+	Translations []Translation
 }
 type Translation struct {
-	Language	string
-	Words	[]Word
+	Language string
+	Words    []Word
 }
 type Word struct {
-	Written	string
-	Sources	[]string
-//	Frequency float32
+	Written string
+	Sources []string
+	//	Frequency float32
+}
+
+func ridx(s string) string {
+	r := regexp.MustCompile("(.*) /.*").FindStringSubmatch(s)
+	if len(r) > 1 {
+		return r[1]
+	} else {
+		return s
+	}
 }
 
 func konverti(de string) (entry Entry, err error) {
@@ -40,9 +49,9 @@ func konverti(de string) (entry Entry, err error) {
 		// Get word type from English definition
 		match := strings.TrimSpace(regexp.MustCompile(`\([a-z]+\.?\)\ *$`).FindString(sourceline[3]))
 		switch match {
-			case "(subst.)", "(verbo)", "(mallon.)", "(adj.)":
-				entry.Wordtype = strings.Trim(match, "()")
-				sourceline[3] = strings.TrimSuffix(strings.TrimSpace(sourceline[3]), match)
+		case "(subst.)", "(verbo)", "(mallon.)", "(adj.)":
+			entry.Wordtype = strings.Trim(match, "()")
+			sourceline[3] = strings.TrimSuffix(strings.TrimSpace(sourceline[3]), match)
 		}
 	}
 
@@ -53,17 +62,17 @@ func konverti(de string) (entry Entry, err error) {
 		}
 		words := []Word{word}
 		if sourceline[4] != "" {
-			words = append(words, Word{ Written: sourceline[4] })
+			words = append(words, Word{Written: sourceline[4]})
 		} else {
 			match := regexp.MustCompile(`\([^)]+\)`).FindString(sourceline[3])
 			if match != "" {
 				matchsplit := strings.Split(match, ",")
 				for i := 0; i < len(matchsplit); i++ {
-					words = append(words, Word{ Written: strings.Trim(matchsplit[i], "() ") })
+					words = append(words, Word{Written: strings.Trim(matchsplit[i], "() ")})
 				}
 			}
 		}
-		english := Translation{ "en", words}
+		english := Translation{"en", words}
 		entry.Translations = append(entry.Translations, english)
 	}
 
@@ -78,7 +87,7 @@ func konverti(de string) (entry Entry, err error) {
 			}
 		}
 		word := Word{
-			Written: sourceline[5],
+			Written: ridx(sourceline[5]),
 			Sources: sources,
 		}
 		words := []Word{word}
@@ -88,15 +97,15 @@ func konverti(de string) (entry Entry, err error) {
 					sources2 = append(sources2, source)
 				}
 			}
-			words = append(words, Word{ Written: sourceline[10], Sources: sources2, })
+			words = append(words, Word{Written: ridx(sourceline[10]), Sources: sources2})
 		}
 		if sourceline[13] != "" {
 			if sourceline[14] != "" {
-				sources3 = []string{ sourceline[14] }
+				sources3 = []string{sourceline[14]}
 			}
-			words = append(words, Word{ Written: sourceline[13], Sources: sources3, })
+			words = append(words, Word{Written: ridx(sourceline[13]), Sources: sources3})
 		}
-		esperanto := Translation{ "eo", words}
+		esperanto := Translation{"eo", words}
 		entry.Translations = append(entry.Translations, esperanto)
 	}
 
@@ -110,7 +119,9 @@ func konverti(de string) (entry Entry, err error) {
 				sourcesnl = append(sourcesnl, source)
 			}
 		}
-		if sourceline[15] != "" { extrainfo = ", " + sourceline[15]}
+		if sourceline[15] != "" {
+			extrainfo = ", " + sourceline[15]
+		}
 		word := Word{
 			Written: sourceline[16] + extrainfo,
 			Sources: sourcesnl,
@@ -118,12 +129,66 @@ func konverti(de string) (entry Entry, err error) {
 		words := []Word{word}
 		if sourceline[21] != "" {
 			if sourceline[22] != "" {
-				sourcesnl2 = []string{ sourceline[22] }
+				sourcesnl2 = []string{sourceline[22]}
 			}
-			words = append(words, Word{ Written: sourceline[21], Sources: sourcesnl2, })
+			words = append(words, Word{Written: sourceline[21], Sources: sourcesnl2})
 		}
-		dutch := Translation{ "nl", words}
+		dutch := Translation{"nl", words}
 		entry.Translations = append(entry.Translations, dutch)
+	}
+
+	// Getting the French definitions
+	if sourceline[24] != "" {
+		var extrainfofr string
+		var sourcesfr []string
+		var sourcesfr2 []string
+		for _, source := range sourceline[26:29] {
+			if source != "" {
+				sourcesfr = append(sourcesfr, source)
+			}
+		}
+		if sourceline[23] != "" {
+			extrainfofr = ", " + sourceline[23]
+		}
+		word := Word{
+			Written: sourceline[24] + extrainfofr,
+			Sources: sourcesfr,
+		}
+		words := []Word{word}
+		if sourceline[29] != "" {
+			if sourceline[30] != "" {
+				sourcesfr2 = []string{sourceline[30]}
+			}
+			words = append(words, Word{Written: sourceline[29], Sources: sourcesfr2})
+		}
+		french := Translation{"fr", words}
+		entry.Translations = append(entry.Translations, french)
+	}
+
+	// Getting the German definitions
+	if sourceline[32] != "" {
+		var extrainfode string
+		var sourcesde []string
+		var sourcesde2 []string
+		if sourceline[31] != "" {
+			extrainfode = ", " + sourceline[31]
+		}
+		if sourceline[33] != "" {
+			sourcesde = []string{sourceline[33]}
+		}
+		word := Word{
+			Written: sourceline[32] + extrainfode,
+			Sources: sourcesde,
+		}
+		words := []Word{word}
+		if sourceline[34] != "" {
+			if sourceline[35] != "" {
+				sourcesde2 = []string{strings.TrimSpace(sourceline[35])}
+			}
+			words = append(words, Word{Written: sourceline[34], Sources: sourcesde2})
+		}
+		german := Translation{"de", words}
+		entry.Translations = append(entry.Translations, german)
 	}
 
 	return
@@ -133,7 +198,9 @@ func konvertifluon(fluo *bufio.Reader) (terminaro Terminaro, err error) {
 	var result Entry
 	for i, er := fluo.ReadString('\n'); er == nil; i, er = fluo.ReadString('\n') {
 		result, err = konverti(i)
-		if err != nil { return }
+		if err != nil {
+			return
+		}
 		terminaro = append(terminaro, result)
 	}
 	return
@@ -144,21 +211,29 @@ func main() {
 	flag.StringVar(&fileTo, "o", "/tmp/output.json", "Write resulting file to this file.")
 	flag.Parse()
 	var fileFrom = flag.Arg(0)
-	
+
 	filefrom, err := os.Open(fileFrom)
 	defer filefrom.Close()
-	if err != nil { panic(err.Error()) }
+	if err != nil {
+		panic(err.Error())
+	}
 
 	fileto, err := os.Create(fileTo)
 	defer fileto.Close()
-	if err != nil { panic(err.Error()) }
+	if err != nil {
+		panic(err.Error())
+	}
 
 	terminaro, err := konvertifluon(bufio.NewReader(filefrom))
-	if err != nil { panic(err.Error()) }
+	if err != nil {
+		panic(err.Error())
+	}
 
 	jsonencb, err := json.MarshalIndent(terminaro, "", " ")
-	if err != nil { panic(err.Error()) }
-	
+	if err != nil {
+		panic(err.Error())
+	}
+
 	jsonenc := bytes.NewBuffer(jsonencb)
 	fmt.Println(jsonenc)
 }
